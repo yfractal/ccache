35e2d36c2b672ad18eac8552e3df4e23d54d9739
Mike Yang <yfractal@gmail.com>
2024-05-13 22:22:16 +0800

    feat: more example

--- derive/src/lib.rs
+++ derive/src/lib.rs
@@ -8,6 +8,33 @@ pub fn derive(input: TokenStream) -> TokenStream {
     let output = quote! {
         impl MyTrait for #ident {}
     };
-
     output.into()
+}
+
+#[proc_macro_derive(EncodeDecode)]
+pub fn encode_decode_derive(input: TokenStream) -> TokenStream {
+    // Parse the input tokens into a syntax tree
+    let input = parse_macro_input!(input as DeriveInput);
+
+    // Get the name of the struct being derived for
+    let name = &input.ident;
+
+    // Generate the implementation for the trait
+    let expanded = quote! {
+        impl EncodeDecode for #name {
+            type Error = String;
+            type Config = ();
+
+            fn encode_to_string(&self, _config: &Self::Config) -> Result<String, Self::Error> {
+                Ok("test".to_string())
+            }
+
+            fn decode_from_string(_val: &String, _config: &Self::Config) -> Result<String, Self::Error> {
+                Ok("abc".to_string())
+            }
+        }
+    };
+
+    // Return the generated implementation as a token stream
+    TokenStream::from(expanded)
 }
--- src/lib.rs
+++ src/lib.rs
@@ -1,6 +1,8 @@
-use derive::MyTrait;
-
 pub mod in_memory_store;
+pub mod serialize;
+
+use derive::MyTrait;
+use derive::EncodeDecode;
 
 trait MyTrait {
     fn answer() -> i32 {
@@ -11,7 +13,24 @@ trait MyTrait {
 #[derive(MyTrait)]
 struct Foo;
 
+trait EncodeDecode {
+    type Error;
+    type Config;
+
+    fn encode_to_string(&self, config: &Self::Config) -> Result<String, Self::Error>;
+    fn decode_from_string(val: &String, config: &Self::Config) -> Result<String, Self::Error> where Self: Sized;
+}
+
+#[derive(EncodeDecode)]
+pub struct Store {
+    pub coder_config: (),
+}
+
 #[test]
 fn default() {
     assert_eq!(Foo::answer(), 42);
+
+    let store: Store = Store{coder_config: ()};
+    assert_eq!(store.encode_to_string(&store.coder_config).unwrap(), "test".to_string());
+    assert_eq!(Store::decode_from_string(&"".to_string(), &store.coder_config).unwrap(), "abc".to_string());
 }
