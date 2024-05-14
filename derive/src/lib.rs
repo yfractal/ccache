976dc8395e4d64b2325df5a655ab9f2d117d7d1a
Mike Yang <yfractal@gmail.com>
2024-05-14 09:19:12 +0800

    feat: EncodeDecode for rust struct

--- derive/src/lib.rs
+++ derive/src/lib.rs
@@ -1,40 +1,52 @@
-use proc_macro::{self, TokenStream};
+extern crate proc_macro;
+
+use proc_macro::TokenStream;
 use quote::quote;
-use syn::{parse_macro_input, DeriveInput};
+use syn::{parse_macro_input, AttributeArgs, DeriveInput, Meta, NestedMeta};
 
-#[proc_macro_derive(MyTrait)]
-pub fn derive(input: TokenStream) -> TokenStream {
-    let DeriveInput { ident, .. } = parse_macro_input!(input);
-    let output = quote! {
-        impl MyTrait for #ident {}
-    };
-    output.into()
-}
-
+// #[proc_macro_derive(EncodeDecode, attributes(config_type, error_type))]
 #[proc_macro_derive(EncodeDecode)]
 pub fn encode_decode_derive(input: TokenStream) -> TokenStream {
     // Parse the input tokens into a syntax tree
     let input = parse_macro_input!(input as DeriveInput);
 
-    // Get the name of the struct being derived for
     let name = &input.ident;
 
     // Generate the implementation for the trait
     let expanded = quote! {
         impl EncodeDecode for #name {
-            type Error = String;
-            type Config = ();
+        // impl<T: EncodeDecode> EncodeDecode for #name<T> {
+            type Error = bincode::error::EncodeError;
+            type Config = bincode::config::Configuration; // Use the config type provided by the attribute
 
-            fn encode_to_string(&self, _config: &Self::Config) -> Result<String, Self::Error> {
-                Ok("test".to_string())
+            fn encode_to_string(&self, config: Self::Config) -> Result<String, Self::Error> {
+                let bytes = bincode::encode_to_vec(&self, config)?;
+                Ok(base64::encode(bytes))
             }
 
-            fn decode_from_string(_val: &String, _config: &Self::Config) -> Result<String, Self::Error> {
-                Ok("abc".to_string())
+            fn decode_from_string(val: &String, config: Self::Config) -> Result<String, Self::Error> {
+                Ok("str".to_string())
             }
         }
     };
 
     // Return the generated implementation as a token stream
     TokenStream::from(expanded)
+}
+
+// Helper function to extract the Config type from the encode_decode_config attribute
+fn extract_type(field: &str, attrs: &[syn::Attribute]) -> syn::Type {
+    for attr in attrs {
+        if let Ok(meta) = attr.parse_meta() {
+            if let Meta::List(list) = meta {
+                if list.path.is_ident(field) {
+                    if let Some(NestedMeta::Meta(Meta::Path(path))) = list.nested.first() {
+                        return syn::parse_quote!(#path);
+                    }
+                }
+            }
+        }
+    }
+    // Default to `()` if no encode_decode_config attribute is found
+    syn::parse_quote!(())
 }
--- src/lib.rs
+++ src/lib.rs
@@ -1,36 +1,53 @@
 pub mod in_memory_store;
 pub mod serialize;
 
-use derive::MyTrait;
+// use derive::MyTrait;
 use derive::EncodeDecode;
-
-trait MyTrait {
-    fn answer() -> i32 {
-        42
-    }
-}
-
-#[derive(MyTrait)]
-struct Foo;
+use bincode::{config, Decode, Encode};
 
 trait EncodeDecode {
     type Error;
     type Config;
 
-    fn encode_to_string(&self, config: &Self::Config) -> Result<String, Self::Error>;
-    fn decode_from_string(val: &String, config: &Self::Config) -> Result<String, Self::Error> where Self: Sized;
+    fn encode_to_string(&self, config: Self::Config) -> Result<String, Self::Error>;
+    fn decode_from_string(val: &String, config: Self::Config) -> Result<String, Self::Error> where Self: Sized;
 }
 
-#[derive(EncodeDecode)]
-pub struct Store {
-    pub coder_config: (),
+// expect
+// #[derive(EncodeDecode)]
+// #[ruby]
+// struct A {}
+// it has ruby implemention
+
+//  #[rust]
+// struct A {}
+// it has rust implemention
+
+
+#[derive(Encode, Decode, EncodeDecode)]
+pub struct Store2{
+    a: bool
+}
+
+impl Store2 {
+    pub fn new() -> Self {
+        Store2 {
+            a: true,
+        }
+    }
 }
 
 #[test]
 fn default() {
-    assert_eq!(Foo::answer(), 42);
+    let store: Store2  = Store2::new();
+    let config = config::standard();
+    let encoded =  store.encode_to_string(config).unwrap();
+    print!("encoded {:?}", encoded);
 
-    let store: Store = Store{coder_config: ()};
-    assert_eq!(store.encode_to_string(&store.coder_config).unwrap(), "test".to_string());
-    assert_eq!(Store::decode_from_string(&"".to_string(), &store.coder_config).unwrap(), "abc".to_string());
+    // assert_eq!(Foo::answer(), 42);
+    // let store = Store{}
+
+    // let store: Store = Store{coder_config: ()};
+    // assert_eq!(store.encode_to_string(&store.coder_config).unwrap(), "test".to_string());
+    // assert_eq!(Store::decode_from_string(&"".to_string(), &store.coder_config).unwrap(), "abc".to_string());
 }
