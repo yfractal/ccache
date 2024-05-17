28a6bec9b9be20e63740bc72c457c120789dd7b4
Mike Yang <yfractal@gmail.com>
2024-05-17 10:12:04 +0800

    feat: init ruby example

---
+++ examples/ruby_example/lib/ruby_example.rb
@@ -0,0 +1,6 @@
+require "ruby_example/version"
+
+module RubyExample
+  class Error < StandardError; end
+  # Your code goes here...
+end
---
+++ examples/ruby_example/spec/ruby_example_spec.rb
@@ -0,0 +1,9 @@
+RSpec.describe RubyExample do
+  it "has a version number" do
+    expect(RubyExample::VERSION).not_to be nil
+  end
+
+  it "does something useful" do
+    expect(false).to eq(true)
+  end
+end
