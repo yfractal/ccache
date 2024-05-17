81a1d624cb68161b385357c7944538ded1cfcbd7
Mike Yang <yfractal@gmail.com>
2024-05-17 17:09:31 +0800

    feat: remove benches

--- derive/src/lib.rs
+++ derive/src/lib.rs
@@ -12,25 +12,22 @@ struct Opts {
 
 #[proc_macro_derive(Serializable, attributes(encode_decode))]
 pub fn encode_decode_derive(input: TokenStream) -> TokenStream {
-    // Parse the input tokens into a syntax tree
     let input = parse_macro_input!(input as DeriveInput);
 
     let name = &input.ident;
 
     let opts = Opts::from_derive_input(&input).expect("Wrong options");
 
-    // Generate the implementation for the trait
     let lan = opts.lan.unwrap_or("rust".to_string());
     if lan == "rust" {
         let expanded = quote! {
             impl Serializable for #name {
                 type Error = bincode::error::EncodeError;
                 type DecodeError = bincode::error::DecodeError;
-                type Config = bincode::config::Configuration; // Use the config type provided by the attribute
+                type Config = bincode::config::Configuration;
 
                 fn encode_to_string(&self, config: &Self::Config) -> Result<String, Self::Error> {
                     let bytes = bincode::encode_to_vec(&self, *config)?;
-                    println!("rust encode_to_string");
                     Ok(base64::encode(bytes))
                 }
 
@@ -74,6 +71,4 @@ pub fn encode_decode_derive(input: TokenStream) -> TokenStream {
 
         TokenStream::from(expanded)
     }
-
-    // Return the generated implementation as a token stream
 }
