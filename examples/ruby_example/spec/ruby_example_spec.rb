0b22d6214d466ebc0d93cbc549425d297ddf08ea
Mike Yang <yfractal@gmail.com>
2024-05-17 16:04:14 +0800

    feat: added ruby tests

--- examples/ruby_example/spec/ruby_example_spec.rb
+++ examples/ruby_example/spec/ruby_example_spec.rb
@@ -1,9 +1,30 @@
 RSpec.describe RubyExample do
-  it "has a version number" do
-    expect(RubyExample::VERSION).not_to be nil
-  end
+  describe 'RubyStore' do
+    let(:ruby_store) {
+      RubyStore.new("redis://127.0.0.1/")
+    }
 
-  it "does something useful" do
-    expect(false).to eq(true)
+    class Foo
+      attr_reader :a, :b
+
+      def initialize(a, b)
+        @a, @b = a, b
+      end
+    end
+
+    it 'works for simple obj' do
+      ruby_store.insert('some-key', true)
+      expect(ruby_store.get('some-key')).to eq true
+    end
+
+    it 'works for obj' do
+      foo = Foo.new(1, "b")
+      ruby_store.insert('some-key', foo)
+      fetched = ruby_store.get('some-key')
+
+      expect(fetched.a).to eq 1
+      expect(fetched.b).to eq 'b'
+      expect(fetched).to eq foo
+    end
   end
 end
