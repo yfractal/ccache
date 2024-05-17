1fff62a6432ae0fe59e01c2971e011132f5c409f
Mike Yang <yfractal@gmail.com>
2024-05-17 11:49:13 +0800

    feat: init ruby example

--- examples/ruby_example/src/lib.rs
+++ examples/ruby_example/src/lib.rs
@@ -1,7 +1,11 @@
+#[macro_use]
+extern crate rutie;
+
+use rutie::{Class, Boolean, Object, RString, VM};
+
 use derive::Serializable;
 use nimbus::in_memory_store::InMemoryStore;
 use nimbus::serializer::Serializable;
-use rutie::{Boolean, Object, RString, VM};
 
 #[derive(Serializable)]
 #[encode_decode(lan = "ruby")]
@@ -9,6 +13,35 @@ pub struct RubyObject {
     pub value: rutie::types::Value,
 }
 
+class!(RutieExample);
+
+methods!(
+    RutieExample,
+    _rtself,
+
+    fn pub_reverse(input: RString) -> RString {
+        let ruby_string = input.
+          map_err(|e| VM::raise_ex(e) ).
+          unwrap();
+
+        RString::new_utf8(
+          &ruby_string.
+          to_string().
+          chars().
+          rev().
+          collect::<String>()
+        )
+    }
+);
+
+#[allow(non_snake_case)]
+#[no_mangle]
+pub extern "C" fn Init_ruby_example() {
+    // RutieExample.reverse "abc"
+    Class::new("RutieExample", None).define(|klass| {
+        klass.def_self("reverse", pub_reverse);
+    });
+}
 
 #[cfg(test)]
 mod tests {
